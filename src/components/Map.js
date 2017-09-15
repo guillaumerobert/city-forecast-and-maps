918589a1dbb54aa405c32f940e497386f4027cdd
Guillaume Robert <guillaume.robert.31410@gmail.com>
2017-09-15 16:07:32 +0200

    Add a basic map with leaflet react

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import '../App.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-class Map extends Component{
+class MapShow extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {position:[51.505,-0.09]};
+    }
+
     render(){
         return(
-        <div>The map page</div>
+        <div className="leafletmap">
+            <h3>My Map Of London :</h3>
+            <Map center={this.state.position} zoom={13}>
+                <TileLayer
+                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={this.state.position}>
+                    <Popup>
+                        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                    </Popup>
+                </Marker>
+            </Map>
+        </div>
         )
     };
 }
 
-export default Map;
+export default MapShow;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import Forecast from './components/Forecast';
-import Map from './components/Map';
+import MapShow from './components/Map';
 
 
 export default (
     <Route path="/" component={App}> // Racine de l'application : le render de App est appelé. Toujours laisser un composant "racine", et plutot definir un IndexRouter
-        <Route path="map" component={Map}/>
+        <Route path="map" component={MapShow}/>
         <Route path="forecast" component={Forecast} /> // Nested routes.
     </Route>
 );
