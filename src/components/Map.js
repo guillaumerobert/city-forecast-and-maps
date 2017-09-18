861e849559ba0e9f6644da546b4fa2b4bab46ba4
Guillaume Robert <guillaume.robert.31410@gmail.com>
2017-09-18 15:58:15 +0200

    Load other cities map with google geocode

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,25 +2,42 @@ import React, {Component} from 'react';
 import '../App.css';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import Control from 'react-leaflet-control';
-import googleCodeAddress from '../apis/googleGeoCode';
+import axios from 'axios';
 
 class MapShow extends Component{
 
     constructor(props){
         super(props);
         this.state = {position:[51.505,-0.09],city:"London"};
+        this.geocodeNewAddress = this.geocodeNewAddress.bind(this);
     }
 
     onInputChange(value){
-        console.log(value);
         this.setState({city:value});
         this.geocodeNewAddress(value);
     }
 
+
+    /**
+     * Dynamically geocode adress given in search bar (City Name).
+     * Using Google Maps Geocode Api.
+     * @param address
+     * @returns {*|Promise.<T>}
+     */
     geocodeNewAddress(address){
-        const encodedAddr = googleCodeAddress(address);
-        console.log(encodedAddr);
-    }
+
+        let self = this;
+        const BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
+        const API_KEY = "AIzaSyARu2bUls2TDAg6uzg85ra9SEhrX61Mr7c"; // Please use your own Key !
+        const fullUrl = `${BASE_URL}address=${address}&key=${API_KEY}`;
+
+        return  axios.get(fullUrl).then(function(response){
+            const coords = response.data.results[0].geometry.location
+            self.setState({position:[coords.lat,coords.lng]});
+        }).catch(function(error){
+            console.log(error);
+        });
+    };
 
     render(){
         return(
