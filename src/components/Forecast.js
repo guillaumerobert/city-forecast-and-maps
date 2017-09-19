6b81cb16685615c21efbfc952c7b7ee8e987a345
Guillaume Robert <guillaume.robert.31410@gmail.com>
2017-09-19 10:04:42 +0200

    prevent default form submit and prepare fetch

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
 
 class Forecast extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = { city: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({city : event.target.value});
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
+
+        /** TODO
+         * Fetch Data
+         */
+        
+    }
+
+
     render(){
         return(
             <div className="container">
                 <h1>Forecast is coming</h1>
-                <form className="input-group">
-                    <input />
+                <form onSubmit={this.onFormSubmit} className="input-group">
+                    <input
+                    placeholder="Choose a city to show its forecast"
+                    className="form-control"
+                    value={this.state.city}
+                    onChange={this.onInputChange}
+                    />
                     <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
