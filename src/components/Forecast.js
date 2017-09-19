980489eeea79a2716b31e0e27aca11f54faff13c
Guillaume Robert <guillaume.robert.31410@gmail.com>
2017-09-19 09:24:58 +0200

    Init forecast page and refine css

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -3,7 +3,16 @@ import React, {Component} from 'react';
 class Forecast extends Component{
     render(){
         return(
-            <div>The forecast page</div>
+            <div className="container">
+                <h1>Forecast is coming</h1>
+                <form className="input-group">
+                    <input />
+                    <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+                </form>
+            </div>
+
         )
     };
 }
