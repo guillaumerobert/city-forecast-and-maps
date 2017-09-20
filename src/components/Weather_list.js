ef6eeeadbfe1f7e0de4d7e0d70d75109108c38df
Guillaume Robert <guillaume.robert.31410@gmail.com>
2017-09-20 14:22:20 +0200

    Simple graphs with sparklines

--- src/components/Weather_list.js
+++ src/components/Weather_list.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import _ from 'lodash';
 
 
 class WeatherList extends Component{
 
+
     renderWeather(cityData){
         const cityName = cityData.data.city.name;
-        const temperature = cityData.data.list[0].main.temp; // temperature comes in kelvin.
-        const pressure = cityData.data.list[0].main.pressure;
-        const humidity = cityData.data.list[0].main.humidity;
+        const temperature = cityData.data.list.map(weather => weather.main.temp); // temperature comes in kelvin.
+        const pressure = cityData.data.list.map(weather => weather.main.pressure);
+        const humidity = cityData.data.list.map(weather => weather.main.humidity);
         return(
             <tr key={cityName}>
                 <td>{cityName}</td>
-                <td>{ Number((temperature - 273).toFixed(1))}</td>
-                <td>{pressure}</td>
-                <td>{humidity}</td>
+                <td>
+                    <Sparklines height="120" width="180" data={temperature}>
+                        <SparklinesLine color="red" />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                    <div>{_.round((_.sum(temperature)/temperature.length)-273)} C°</div>
+                </td>
+                <td>
+                    <Sparklines height="120" width="180" data={pressure}>
+                        <SparklinesLine color="green" />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                    <div>{_.round(_.sum(pressure)/pressure.length)} kPa</div>
+                </td>
+                <td>
+                    <Sparklines height="120" width="180" data={humidity}>
+                        <SparklinesLine color="blue" />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                    <div>{_.round(_.sum(humidity)/humidity.length)} %</div>
+                </td>
             </tr>
         );
     }
